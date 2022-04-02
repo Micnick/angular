023331aa97b6d9cea84b34c370ebe0e67002d83d
Leoromero.Dev <leonardoromerocastro@gmail.com>
2022-04-02 14:42:49 -0500

    First Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
